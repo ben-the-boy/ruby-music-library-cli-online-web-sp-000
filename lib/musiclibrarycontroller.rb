30db4ea01ce4937898ccbb9ddff02aca100bbb25
Ben Hayes <b.t.hayes@comcast.net>
2021-01-21 23:15:45 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -17,7 +17,9 @@ class MusicLibraryController
     puts "What would you like to do?"
     input = gets.strip 
     if input != "exit"
-      call
+      if input == "list songs"
+        list_songs 
+      end 
     end 
   end
   
